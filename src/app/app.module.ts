e3f70643ddc915376410b3f1b134bb2e29e85034
Dmitriy24S <devilins24@gmail.com>
2023-09-24 16:01:46 +0300

    feat: add Employee form values binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatRadioModule } from '@angular/material/radio';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, EmpAddEditComponent],
@@ -32,6 +33,7 @@ import { MatRadioModule } from '@angular/material/radio';
     MatNativeDateModule,
     MatSelectModule,
     MatRadioModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/emp-add-edit/emp-add-edit.component.ts
+++ src/app/emp-add-edit/emp-add-edit.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { DateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-emp-add-edit',
@@ -6,6 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./emp-add-edit.component.scss'],
 })
 export class EmpAddEditComponent {
+  empForm: FormGroup;
+  formattedDate: string;
+
   education: string[] = [
     'Matric',
     'Diploma',
@@ -13,4 +18,39 @@ export class EmpAddEditComponent {
     'Graduate',
     'Post Graduate',
   ];
+
+  // service variable
+  constructor(
+    private _fb: FormBuilder,
+    private _dateAdapter: DateAdapter<Date>
+  ) {
+    this.empForm = this._fb.group({
+      firstName: '1',
+      lastName: '2',
+      email: '3',
+      dob: '05/05/1993',
+      gender: 'male',
+      education: 'Graduate',
+      company: '7',
+      experience: '8',
+      package: '9',
+    });
+
+    const date = new Date('05/05/1993');
+    this.empForm.get('dob')?.setValue(date);
+    this.formattedDate = this._dateAdapter.format(date, 'MM-dd-yyyy');
+  }
+
+  onFormSubmit() {
+    if (this.empForm.valid) {
+      console.log('1', this.empForm.value);
+      const dateObj = new Date(this.empForm.value.dob);
+      // const dateObj = new Date(this.empForm.get('dob'));
+      const formattedDate = `${
+        dateObj.getMonth() + 1
+      }/${dateObj.getDate()}/${dateObj.getFullYear()}`;
+      // this.empForm.get('dob')?.setValue(formattedDate);
+      console.log('2', { ...this.empForm.value, dob: formattedDate });
+    }
+  }
 }
