aebb69f397e1ac0abeb8038d7888163f19dac9bc
Dmitriy24S <devilins24@gmail.com>
2023-09-25 14:05:21 +0300

    feat: add getEmployeeList ngOnInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
+import { EmployeeService } from './services/employee.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+
+// OnInit is a lifecycle hook that is called after Angular has initialized all data-bound properties of a directive. Define an ngOnInit() method to handle any additional initialization tasks.
+export class AppComponent implements OnInit {
   title = 'angular-crud-1';
 
   // dep injection, service variable
-  constructor(private _dialog: MatDialog) {
+  constructor(
+    private _dialog: MatDialog,
+    private _empService: EmployeeService
+  ) {
     //
+  }
+
+  ngOnInit(): void {
+    this.getEmployeeList();
   }
 
   // openDialog(): void {
@@ -23,9 +33,24 @@ export class AppComponent {
   // });
   // }
 
+  // methods:
   openAddEditEmpForm() {
     this._dialog.open(EmpAddEditComponent, {
       // autoFocus: false,
+    });
+  }
+
+  getEmployeeList() {
+    // returns observable, so subscribe and handle it
+    this._empService.getEmployeeList().subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+      // error(err) {
+      // error: (err) => {
+      //   console.log('getEmployeeList err', err);
+      // },
+      error: console.log,
     });
   }
 }
